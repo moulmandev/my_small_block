5abd5e2b39e2c4f2f4043d971730f81cd148d095
Antoine <antoine_pannecoucke@orange.fr>
2021-04-02 12:16:46 +0200

    Systeme de filtre sur la page index

--- src/Controller/ShopsController.php
+++ src/Controller/ShopsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use \Cake\ORM\Query;
 use App\Controller\AppController;
 
 class ShopsController extends AppController
@@ -12,13 +13,33 @@ class ShopsController extends AppController
     }
 
     public function index() {
+        //TODO filters
         $modsLocator = $this->getTableLocator()->get('Mods');
-
-        $modsArray = $modsLocator->find()
-            ->limit(6)
+        $keywordsLocator = $this->getTableLocator()->get('Keywords');
+        $keywords = $keywordsLocator->find()
+            ->select(['key'])
+            ->group('keywords.`key`')
             ->toArray();
+        $params = $this->request->getQuery();
+        if (!empty($params)){
 
-        $this->set(compact("modsArray"));
+            $modsArray = $modsLocator->find()
+                ->innerJoinWith('Keywords', function (Query $q) use ($params){
+                    return $q
+                        ->where(['Keywords.key' => $params['key']]);
+                })
+                ->limit(6)
+                ->toArray();
+            
+        }
+        else {
+
+            $modsArray = $modsLocator->find()
+                ->limit(6)
+                ->toArray();
+        }
+
+        $this->set(compact("modsArray", 'keywords'));
     }
 
     public function cart() {
