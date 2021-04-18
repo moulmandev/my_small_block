4d1f57e1e5b4ad9055fe546d9df377ed3e461981
Théo Escolano <contact@moulmandev.fr>
2021-04-18 14:39:31 +0200

    add mod front update

--- templates/Admins/add.php
+++ templates/Admins/add.php
@@ -1,32 +1,50 @@
-<?= $this->Form->create($modEntity, ['url' => ['controller' => 'Admins', 'action' => 'add']])?>
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">AJOUTER UN MOD</h1>
+    </div>
+</section>
 
-<?= $this->Form->control('name', [
-    'label' => 'Nom',
-    'class' => 'form-control'
-])?>
+<div class="container mb-4">
+    <div class="row">
+        <div class="col-12">
 
-<?= $this->Form->control('description', [
-    'type' => 'text',
-    'label' => 'Description',
-    'class' => 'form-control'
-])?>
+            <?= $this->Form->create($modEntity, ['url' => ['controller' => 'Admins', 'action' => 'add']])?>
 
-<?= $this->Form->control('picture', [
-    'type' => 'text',
-    'label' => 'Url de l\'image',
-    'class' => 'form-control'
-])?>
+            <?= $this->Form->control('name', [
+                'label' => 'Nom',
+                'class' => 'form-control'
+            ])?>
 
-<?= $this->Form->control('price', [
-    'type' => 'decimal',
-    'label' => 'Prix',
-    'class' => 'form-control'
-])?>
+            <?= $this->Form->control('description', [
+                'type' => 'text',
+                'label' => 'Description',
+                'class' => 'form-control'
+            ])?>
 
-<?= $this->Form->button('Valider', [
-    'type' => 'submit',
-    'escapeTitle' => false,
-    'class' => 'btn btn-primary'
-]);?>
+            <?= $this->Form->control('picture', [
+                'type' => 'text',
+                'label' => 'Url de l\'image',
+                'class' => 'form-control'
+            ])?>
 
-<?= $this->Form->end(); ?>
+            <?= $this->Form->control('price', [
+                'type' => 'decimal',
+                'label' => 'Prix',
+                'class' => 'form-control'
+            ])?>
+
+            <?= $this->Form->button('Valider', [
+                'type' => 'submit',
+                'escapeTitle' => false,
+                'class' => 'btn btn-primary'
+            ]);?>
+
+            <?= $this->Form->end(); ?>
+
+        </div>
+    </div>
+</div>
+
+
+
+
